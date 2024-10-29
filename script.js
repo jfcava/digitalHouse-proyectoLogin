2ce854fe7d1122d748af8e7fe24ccb1ce7cfa354
Juan Cavallieri <juancavallieri@gmail.com>
2024-10-29 16:58:53 -0300

    Proyecto Completo de Validacion de un Login

--- script.js
+++ script.js
@@ -6,35 +6,108 @@ document.addEventListener('DOMContentLoaded', function(){
     const confirmPasswordInput = document.getElementById('confirmPassword');
     const emailError = document.getElementById('emailError');
     const passwordError = document.getElementById('passwordError');
-    const confirmpasswordError = document.getElementById('confirmpasswordError');
+    const confirmPasswordError = document.getElementById('confirmPasswordError');
+    const showHideButton = document.getElementById('show-hide')
 
     loginForm.addEventListener('submit', function(event){
         event.preventDefault();
-        //TODO: agregar metodo que valide el formulario
+        validateForm()
     })
 
     emailInput.addEventListener('blur', function(){ // blur es un evento cuando salimos del formulario
-        //TODO: agregar metodo que valide el mail
+        validateEmail()
     })
 
     emailInput.addEventListener('change', function(){ // change es cuando hay un cambio, ej cuando escribo algo
-        //TODO: agregar metodo que limpie el error
+        clearError(emailError)
     })
 
     passwordInput.addEventListener('change', function(){ // change es cuando hay un cambio, ej cuando escribo algo
-        //TODO: agregar metodo que limpie el error
+        clearError(passwordError)
     })
 
     confirmPasswordInput.addEventListener('change', function(){ // change es cuando hay un cambio, ej cuando escribo algo
-        //TODO: agregar metodo que limpie el error
+        clearError(confirmpasswordError)
+    })
+
+    showHideButton.addEventListener('click', function(){
+        if(passwordInput.type == 'password'){
+            passwordInput.type = 'text'
+            confirmPasswordInput.type = 'text'
+        }else{
+            passwordInput.type = 'password'
+            confirmPasswordInput.type = 'password'
+        }
     })
 
     function validateForm(){
+        const isValidEmail = validateEmail()
+        const isValidPassword = validatePassword()
+        const passwordMatch = validatePasswordMatch()
 
+        if(isValidEmail && isValidPassword && passwordMatch){
+            saveToLocalStorage();
+            alert('Has ingresado con exito');
+        }
     }
 
     function validateEmail(){
-        const regex = 
+        const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+        const emailValue = emailInput.value.trim() // el trim elimina espacios vacios al comienzo y final
+
+        if(!emailRegex.test(emailValue)){
+            showError(emailError, 'Ingresa un email valido')
+            return false
+        }
+
+        return true;
     }
+
+    function validatePassword(){
+        const passwordValue = passwordInput.value.trim()
+
+        if(passwordValue.length < 6){
+            showError(passwordError, 'Ingresa una contraseña de al menos 6 caracteres')
+            return false;
+        }
+        return true;
+    }
+
+    function validatePasswordMatch(){
+        const passwordValue = passwordInput.value.trim()
+        const confirmPasswordValue = confirmPasswordInput.value.trim()
+
+        if(passwordValue != confirmPasswordValue){
+            showError(confirmPasswordError, 'Las contraseñas no coinciden')
+            return false
+        }
+        return true
+    }
+
+    function showError(errorElement, message){
+        errorElement.innerHTML = message;
+        errorElement.style.display = 'block';
+    }
+    
+    function clearError(errorElement){
+        errorElement.innerHTML = '';
+        errorElement.style.display = '';
+    }
+
+    function saveToLocalStorage(){
+        const emailValue = emailInput.value.trim();
+        localStorage.setItem('email', emailValue);
+        const body = bodyBuilderJSON();
+        console.log(body);
+    }
+
+    function bodyBuilderJSON(){
+        return {
+            "email": emailInput.value,
+            "password": passwordInput.value
+        }
+    }
+
+
 
 })
